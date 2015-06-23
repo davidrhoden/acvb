d8cc165eda3db24c9c1c83765184f7ecb504b4e1
David Rhoden <david@davidrhoden.com>
2015-06-23 17:20:57 -0500

    init commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Quiz: Find your perfect Austin taco</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link href='http://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    </head>
+    <body>
+    <div id="wrapper">
+        <h1>Quiz: Find your perfect Austin taco</h1>
+
+<p class="q">Question 1: Your personal taco philosophy is:</p>
+<p class="a"><span class="quiz_checkbox quiz-checkbox fa-square-o"></span> A.	The fanciest, fusioniest, most gourmet ingredients I can find.</p>
+<p class="a"><span class="quiz_checkbox quiz-checkbox fa-square-o"></span> B.	Fresh. Simple. YUM.</p>
+<p class="a"><span class="quiz_checkbox quiz-checkbox fa-square-o"></span> C.	Customization. Every taco should be constructed to my taste bud’s desires.</p>
+<p class="a"><span class="quiz_checkbox quiz-checkbox fa-square-o"></span> D.	Big and messy. Load ‘em up and sauce ‘em down.</p>
+
+<p class="q">Question 2: How do you eat a taco?</p>
+<p class="a"><span class="quiz_checkbox quiz-checkbox fa-square-o"></span> A.	I’m not a savage, I use a knife and fork.</p>
+<p class="a"><span class="quiz_checkbox quiz-checkbox fa-square-o"></span> B.	With my hands wrapped around a CORN tortilla.</p>
+<p class="a"><span class="quiz_checkbox quiz-checkbox fa-square-o"></span> C.	With my hands wrapped around a FLOUR tortilla.</p>
+<p class="a"><span class="quiz_checkbox quiz-checkbox fa-square-o"></span> D.	Like this: nom-nom-nom-nom-nom.</p>
+
+<p class="q">Question 3: It’s Taco Time! You:</p>
+<p class="a"><span class="quiz_checkbox quiz-checkbox fa-square-o"></span> A.	Get gussied up for a fancy taco dinner date for two.</p>
+<p class="a"><span class="quiz_checkbox quiz-checkbox fa-square-o"></span> B.	Commune (and consume) out of doors with like-minded taco tasters.</p>
+<p class="a"><span class="quiz_checkbox quiz-checkbox fa-square-o"></span> C.	Fire up Netflix – these tacos are getting delivered to me!</p>
+<p class="a"><span class="quiz_checkbox quiz-checkbox fa-square-o"></span> D.	Speed to my favorite taco place – no time to waste.</p>
+
+<p class="q">Question 4: Do you love tacos?</p>
+<p class="a"><span class="quiz_checkbox quiz-checkbox fa-square-o"></span> A.	Yes, I’d marry them if I could. Wait, can I?</p>
+<p class="a"><span class="quiz_checkbox quiz-checkbox fa-square-o"></span> B.	Yep. I &lt;3 tacos.</p>
+<p class="a"><span class="quiz_checkbox quiz-checkbox fa-square-o"></span> C.	Of course. And I’m offended you even asked.</p>
+<p class="a"><span class="quiz_checkbox quiz-checkbox fa-square-o"></span> D.	No. (Just kidding, of course I do. TACOS!!!)</p>
+</div>
+    </body>
+</html>
