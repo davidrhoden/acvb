3c7bba627e6a7e1f0ac47d011e275dbdd83ba4b1
David Rhoden <david@davidrhoden.com>
2015-06-26 10:56:08 -0500

    add share buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 var points, select, thisid, thisrel, choices, resultOne, resultTwo, resultThree, ResultFour;
 points = 0;
 
-resultOne = "<h2>Fork &amp; Taco</h2>Oh you fancy huh? Get your taste buds ready to dive into Asian Pear Chicken and Thai Grouper tacos, just make sure to use your dinner fork. Because tacos with a dessert fork – that’s just weird.";
+resultOne = "<h2>Fork &amp; Taco</h2><p>Oh you fancy huh? Get your taste buds ready to dive into Asian Pear Chicken and Thai Grouper tacos, just make sure to use your dinner fork. Because tacos with a dessert fork – that’s just weird.</p><p>Share this quiz! <i class=\"fa fa-facebook-official\"></i><i class=\"fa fa-twitter-square\"></i></p>";
 
-resultTwo = "<h2>Rositas Taco</h2>You like your tacos fresh, tasty and as authentic as they come. Next time you’re in town, get ready for the best food truck taco you’ve ever jammed down your mouth hole. ";
+resultTwo = "<h2>Rositas Taco</h2><p>You like your tacos fresh, tasty and as authentic as they come. Next time you’re in town, get ready for the best food truck taco you’ve ever jammed down your mouth hole. </p><p>Share this quiz! <i class=\"fa fa-facebook-official\"></i><i class=\"fa fa-twitter-square\"></i></p>";
 
-resultThree = "<h2>Taco Deli</h2>You’re headed to the deli! Don’t worry, no sandwiches (or tortas) here. Just classic – and completely delicious – Austin tacos. Pro tip: accessorize with plenty of the legendary green sauce. ";
+resultThree = "<h2>Taco Deli</h2><p>You’re headed to the deli! Don’t worry, no sandwiches (or tortas) here. Just classic – and completely delicious – Austin tacos. Pro tip: accessorize with plenty of the legendary green sauce. </p><p>Share this quiz! <i class=\"fa fa-facebook-official\"></i><i class=\"fa fa-twitter-square\"></i></p>";
 
-resultFour = "<h2>Torchy’s Tacos</h2>You’ve elected Torchy’s as President of Austin Tacos. But whether your mouth votes Democrat or Republican (those are both taco names), you’re sure to go home happy. ";
+resultFour = "<h2>Torchy’s Tacos</h2><p>You’ve elected Torchy’s as President of Austin Tacos. But whether your mouth votes Democrat or Republican (those are both taco names), you’re sure to go home happy. </p><p>Share this quiz! <i class=\"fa fa-facebook-official\"></i><i class=\"fa fa-twitter-square\"></i></p>";
 
 
 function turnBlue(){
